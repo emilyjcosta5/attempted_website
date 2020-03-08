70cd2ae2f4211c4901ff021f59451710e48b3da3
Emily Costa <ecost020@fiu.edu>
2020-03-08 19:48:15 -0400

    Web dev is no fun

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import './bg.css';
+import fiu_logo from './fiu-white-logo.png';
 
 function App() {
   return (
-    <div>
+    <html>
+     <body>
       <h1>Emily Costa</h1>
-    </div>
+     </body>
+    </html>
+
   );
 }
 
